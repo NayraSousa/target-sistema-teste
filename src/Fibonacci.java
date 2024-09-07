34f3893f450e31ef0a4b8ef17a6fe1d728d96d85
NayraSousa <nayraamaral1@gmail.com>
2024-09-06 21:18:31 -0300

    feat: add fibonacci algorithm

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Fibonacci {
+
+    public static List<Integer> fibonacci(int n){
+
+        int first_number = 0;
+        int second_number = 1;
+        int result = 0;
+        List<Integer> sequence = new ArrayList<>();
+
+        for(int count = 0; count < n; count++){
+
+            result = first_number+second_number;
+            sequence.add(result);
+            first_number = second_number;
+            second_number = result;
+        }
+        return sequence;
+    }
+
+    public static void main(String[] args){
+
+        int number = 8;
+        List<Integer> result = fibonacci(number);
+
+        System.out.println(result);
+
+        if(result.contains(number)){
+            System.out.println("Result contains number");
+        } else {
+            System.out.println("Result doesn't contains number");
+
         }
     }
+
 }
